5127d6b2667759de9aac09e37d3fd776b5d28792
ji hyeon woo <908dkwmdk@naver.com>
2022-09-21 14:12:41 +0900

    carousel script add
    리뷰 화살표 동작 구현

--- script/main.js
+++ script/main.js
@@ -6,7 +6,7 @@ window.addEventListener("DOMContentLoaded", function(){
 	const CLOSE = document.querySelector("#close");
 				
 	function showAfterLoad(){
-// 		console.log(popupOuter);	// .log : 콘솔에 출력명령
+ 		/*console.log(popupOuter);	// .log : 콘솔에 출력명령*/
 		//console.dir(popupOuter);	// 대상의 객체에 정보를 탐색
 		popupOuter.style.display = "block";
 					
@@ -18,7 +18,38 @@ window.addEventListener("DOMContentLoaded", function(){
 		POPUP.style.display = "none";
 		BODY.style.overflow = "auto";
 	})
-					
+	
+	//.prev 버튼 누르면 현재 캐러셀 이미지 I -> II의 위치로 조작
+	const PREV = document.querySelector(".prev");
+	const NEXT = document.querySelector(".next");
+	const POSTS = document.querySelector("#posts")
+	
+	let postIndex = 0;
+	
+	
+	PREV.addEventListener("click", function(){
+		postIndex--;
+		
+		postIndex < 0 ? postIndex = 2  : null;
+		
+		console.log(postIndex);
+		
+		let coords = -920 * postIndex;
+		
+		POSTS.style.marginLeft = coords+"px";
+	})
+	
+	NEXT.addEventListener("click", function(){
+		postIndex++;
+		
+		postIndex < 3 ? postIndex = 0 : null;
+		let coords = -920 * postIndex;
+		
+		POSTS.style.marginLeft = coords+"px";
+		//캐러셀 옮기기
+		// 현재 : 0 x-920 = 0		다음 : 1 x -920 = -920	다다음 : 2 x 920 = -1840
+		
+	})			
 })
 				
 		
